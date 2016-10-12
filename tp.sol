2a318afebcddfbf0aee33b8b5fce3363dfea6ba6
Fernando Cancellara <fdcancellara@gmail.com>
2016-10-12 17:34:44 -0300

    Subo output de tp.mod

---
+++ tp.sol
@@ -0,0 +1,381 @@
+Problem:    tp
+Rows:       122
+Columns:    57 (57 integer, 57 binary)
+Non-zeros:  386
+Status:     INTEGER OPTIMAL
+Objective:  z = 37 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 z                          37                             
+     2 debenSerCiertas[condicion1]
+                                   1             1             = 
+     3 debenSerCiertas[condicion2]
+                                   1             1             = 
+     4 debenSerCiertas[condicion3]
+                                   1             1             = 
+     5 debenSerCiertas[condicion4]
+                                   1             1             = 
+     6 debenSerCiertas[condicion5]
+                                   1             1             = 
+     7 debenSerCiertas[condicion6]
+                                   1             1             = 
+     8 debenSerCiertas[condicion7]
+                                   1             1             = 
+     9 debenSerCiertas[condicion8]
+                                   1             1             = 
+    10 debenSerCiertas[condicion9]
+                                   1             1             = 
+    11 debenSerCiertas[condicion10]
+                                   1             1             = 
+    12 debenSerCiertas[condicion11]
+                                   1             1             = 
+    13 debenSerCiertas[condicion12]
+                                   1             1             = 
+    14 debenSerCiertas[condicion13]
+                                   1             1             = 
+    15 debenSerCiertas[condicion14]
+                                   1             1             = 
+    16 debenSerCiertas[condicion15]
+                                   1             1             = 
+    17 debenSerCiertas[condicion16]
+                                   1             1             = 
+    18 debenSerCiertas[condicion17]
+                                   1             1             = 
+    19 debenSerCiertas[condicion18]
+                                   1             1             = 
+    20 debenSerCiertas[condicion19]
+                                   1             1             = 
+    21 debenSerCiertas[condicion20]
+                                   1             1             = 
+    22 debenSerCiertas[condicion21]
+                                   1             1             = 
+    23 debenSerCiertas[condicion22]
+                                   1             1             = 
+    24 debenSerCiertas[condicion23]
+                                   1             1             = 
+    25 debenSerCiertas[condicion24]
+                                   1             1             = 
+    26 debenSerCiertas[condicion25]
+                                   1             1             = 
+    27 debenSerCiertas[condicion26]
+                                   1             1             = 
+    28 debenSerCiertas[condicion27]
+                                   1             1             = 
+    29 debenSerCiertas[condicion28]
+                                   1             1             = 
+    30 debenSerCiertas[condicion29]
+                                   1             1             = 
+    31 debenSerCiertas[condicion30]
+                                   1             1             = 
+    32 debenSerCiertas[condicion31]
+                                   1             1             = 
+    33 debenSerCiertas[condicion32]
+                                   1             1             = 
+    34 debenSerCiertas[condicion33]
+                                   1             1             = 
+    35 debenSerCiertas[condicion34]
+                                   1             1             = 
+    36 debenSerCiertas[condicion35]
+                                   1             1             = 
+    37 debenSerCiertas[condicion36]
+                                   1             1             = 
+    38 debenSerCiertas[condicion37]
+                                   1             1             = 
+    39 condicionesOrLadoIzquierdo[condicion1]
+                                   0            -0               
+    40 condicionesOrLadoIzquierdo[condicion2]
+                                   1            -0               
+    41 condicionesOrLadoIzquierdo[condicion3]
+                                   0            -0               
+    42 condicionesOrLadoIzquierdo[condicion4]
+                                   1            -0               
+    43 condicionesOrLadoIzquierdo[condicion5]
+                                   1            -0               
+    44 condicionesOrLadoIzquierdo[condicion6]
+                                   1            -0               
+    45 condicionesOrLadoIzquierdo[condicion7]
+                                   0            -0               
+    46 condicionesOrLadoIzquierdo[condicion8]
+                                   1            -0               
+    47 condicionesOrLadoIzquierdo[condicion9]
+                                   2            -0               
+    48 condicionesOrLadoIzquierdo[condicion10]
+                                   0            -0               
+    49 condicionesOrLadoIzquierdo[condicion11]
+                                   1            -0               
+    50 condicionesOrLadoIzquierdo[condicion12]
+                                   0            -0               
+    51 condicionesOrLadoIzquierdo[condicion13]
+                                   0            -0               
+    52 condicionesOrLadoIzquierdo[condicion14]
+                                   1            -0               
+    53 condicionesOrLadoIzquierdo[condicion15]
+                                   0            -0               
+    54 condicionesOrLadoIzquierdo[condicion16]
+                                   2            -0               
+    55 condicionesOrLadoIzquierdo[condicion17]
+                                   1            -0               
+    56 condicionesOrLadoIzquierdo[condicion18]
+                                   1            -0               
+    57 condicionesOrLadoIzquierdo[condicion19]
+                                   1            -0               
+    58 condicionesOrLadoIzquierdo[condicion20]
+                                   0            -0               
+    59 condicionesOrLadoIzquierdo[condicion21]
+                                   0            -0               
+    60 condicionesOrLadoIzquierdo[condicion22]
+                                   1            -0               
+    61 condicionesOrLadoIzquierdo[condicion23]
+                                   1            -0               
+    62 condicionesOrLadoIzquierdo[condicion24]
+                                   0            -0               
+    63 condicionesOrLadoIzquierdo[condicion25]
+                                   1            -0               
+    64 condicionesOrLadoIzquierdo[condicion26]
+                                   1            -0               
+    65 condicionesOrLadoIzquierdo[condicion27]
+                                   1            -0               
+    66 condicionesOrLadoIzquierdo[condicion28]
+                                   1            -0               
+    67 condicionesOrLadoIzquierdo[condicion29]
+                                   2            -0               
+    68 condicionesOrLadoIzquierdo[condicion30]
+                                   0            -0               
+    69 condicionesOrLadoIzquierdo[condicion31]
+                                   1            -0               
+    70 condicionesOrLadoIzquierdo[condicion32]
+                                   1            -0               
+    71 condicionesOrLadoIzquierdo[condicion33]
+                                   0            -0               
+    72 condicionesOrLadoIzquierdo[condicion34]
+                                   0            -0               
+    73 condicionesOrLadoIzquierdo[condicion35]
+                                   0            -0               
+    74 condicionesOrLadoIzquierdo[condicion36]
+                                   1            -0               
+    75 condicionesOrLadoIzquierdo[condicion37]
+                                   1            -0               
+    76 condicionesOrLadoDerecho[condicion1]
+                                  -2                          -0 
+    77 condicionesOrLadoDerecho[condicion2]
+                                  -1                          -0 
+    78 condicionesOrLadoDerecho[condicion3]
+                                  -2                          -0 
+    79 condicionesOrLadoDerecho[condicion4]
+                                  -1                          -0 
+    80 condicionesOrLadoDerecho[condicion5]
+                                  -1                          -0 
+    81 condicionesOrLadoDerecho[condicion6]
+                                  -1                          -0 
+    82 condicionesOrLadoDerecho[condicion7]
+                                  -2                          -0 
+    83 condicionesOrLadoDerecho[condicion8]
+                                  -1                          -0 
+    84 condicionesOrLadoDerecho[condicion9]
+                                   0                          -0 
+    85 condicionesOrLadoDerecho[condicion10]
+                                  -2                          -0 
+    86 condicionesOrLadoDerecho[condicion11]
+                                  -1                          -0 
+    87 condicionesOrLadoDerecho[condicion12]
+                                  -2                          -0 
+    88 condicionesOrLadoDerecho[condicion13]
+                                  -2                          -0 
+    89 condicionesOrLadoDerecho[condicion14]
+                                  -1                          -0 
+    90 condicionesOrLadoDerecho[condicion15]
+                                  -2                          -0 
+    91 condicionesOrLadoDerecho[condicion16]
+                                   0                          -0 
+    92 condicionesOrLadoDerecho[condicion17]
+                                  -1                          -0 
+    93 condicionesOrLadoDerecho[condicion18]
+                                  -1                          -0 
+    94 condicionesOrLadoDerecho[condicion19]
+                                  -1                          -0 
+    95 condicionesOrLadoDerecho[condicion20]
+                                  -2                          -0 
+    96 condicionesOrLadoDerecho[condicion21]
+                                  -2                          -0 
+    97 condicionesOrLadoDerecho[condicion22]
+                                  -1                          -0 
+    98 condicionesOrLadoDerecho[condicion23]
+                                  -1                          -0 
+    99 condicionesOrLadoDerecho[condicion24]
+                                  -1                          -0 
+   100 condicionesOrLadoDerecho[condicion25]
+                                  -1                          -0 
+   101 condicionesOrLadoDerecho[condicion26]
+                                   0                          -0 
+   102 condicionesOrLadoDerecho[condicion27]
+                                  -1                          -0 
+   103 condicionesOrLadoDerecho[condicion28]
+                                  -1                          -0 
+   104 condicionesOrLadoDerecho[condicion29]
+                                   0                          -0 
+   105 condicionesOrLadoDerecho[condicion30]
+                                  -2                          -0 
+   106 condicionesOrLadoDerecho[condicion31]
+                                  -1                          -0 
+   107 condicionesOrLadoDerecho[condicion32]
+                                  -1                          -0 
+   108 condicionesOrLadoDerecho[condicion33]
+                                  -2                          -0 
+   109 condicionesOrLadoDerecho[condicion34]
+                                  -2                          -0 
+   110 condicionesOrLadoDerecho[condicion35]
+                                  -2                          -0 
+   111 condicionesOrLadoDerecho[condicion36]
+                                  -1                          -0 
+   112 condicionesOrLadoDerecho[condicion37]
+                                  -1                          -0 
+   113 relacionarNormalesConSuNegada[relA]
+                                   1             1             = 
+   114 relacionarNormalesConSuNegada[relB]
+                                   1             1             = 
+   115 relacionarNormalesConSuNegada[relC]
+                                   1             1             = 
+   116 relacionarNormalesConSuNegada[relD]
+                                   1             1             = 
+   117 relacionarNormalesConSuNegada[relE]
+                                   1             1             = 
+   118 relacionarNormalesConSuNegada[relF]
+                                   1             1             = 
+   119 relacionarNormalesConSuNegada[relG]
+                                   1             1             = 
+   120 relacionarNormalesConSuNegada[relH]
+                                   1             1             = 
+   121 relacionarNormalesConSuNegada[relI]
+                                   1             1             = 
+   122 relacionarNormalesConSuNegada[relJ]
+                                   1             1             = 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 condicionesACumplir[condicion1]
+                    *              1             0             1 
+     2 condicionesACumplir[condicion2]
+                    *              1             0             1 
+     3 condicionesACumplir[condicion3]
+                    *              1             0             1 
+     4 condicionesACumplir[condicion4]
+                    *              1             0             1 
+     5 condicionesACumplir[condicion5]
+                    *              1             0             1 
+     6 condicionesACumplir[condicion6]
+                    *              1             0             1 
+     7 condicionesACumplir[condicion7]
+                    *              1             0             1 
+     8 condicionesACumplir[condicion8]
+                    *              1             0             1 
+     9 condicionesACumplir[condicion9]
+                    *              1             0             1 
+    10 condicionesACumplir[condicion10]
+                    *              1             0             1 
+    11 condicionesACumplir[condicion11]
+                    *              1             0             1 
+    12 condicionesACumplir[condicion12]
+                    *              1             0             1 
+    13 condicionesACumplir[condicion13]
+                    *              1             0             1 
+    14 condicionesACumplir[condicion14]
+                    *              1             0             1 
+    15 condicionesACumplir[condicion15]
+                    *              1             0             1 
+    16 condicionesACumplir[condicion16]
+                    *              1             0             1 
+    17 condicionesACumplir[condicion17]
+                    *              1             0             1 
+    18 condicionesACumplir[condicion18]
+                    *              1             0             1 
+    19 condicionesACumplir[condicion19]
+                    *              1             0             1 
+    20 condicionesACumplir[condicion20]
+                    *              1             0             1 
+    21 condicionesACumplir[condicion21]
+                    *              1             0             1 
+    22 condicionesACumplir[condicion22]
+                    *              1             0             1 
+    23 condicionesACumplir[condicion23]
+                    *              1             0             1 
+    24 condicionesACumplir[condicion24]
+                    *              1             0             1 
+    25 condicionesACumplir[condicion25]
+                    *              1             0             1 
+    26 condicionesACumplir[condicion26]
+                    *              1             0             1 
+    27 condicionesACumplir[condicion27]
+                    *              1             0             1 
+    28 condicionesACumplir[condicion28]
+                    *              1             0             1 
+    29 condicionesACumplir[condicion29]
+                    *              1             0             1 
+    30 condicionesACumplir[condicion30]
+                    *              1             0             1 
+    31 condicionesACumplir[condicion31]
+                    *              1             0             1 
+    32 condicionesACumplir[condicion32]
+                    *              1             0             1 
+    33 condicionesACumplir[condicion33]
+                    *              1             0             1 
+    34 condicionesACumplir[condicion34]
+                    *              1             0             1 
+    35 condicionesACumplir[condicion35]
+                    *              1             0             1 
+    36 condicionesACumplir[condicion36]
+                    *              1             0             1 
+    37 condicionesACumplir[condicion37]
+                    *              1             0             1 
+    38 binariasTodas[a]
+                    *              0             0             1 
+    39 binariasTodas[b]
+                    *              0             0             1 
+    40 binariasTodas[c]
+                    *              1             0             1 
+    41 binariasTodas[d]
+                    *              0             0             1 
+    42 binariasTodas[e]
+                    *              1             0             1 
+    43 binariasTodas[f]
+                    *              1             0             1 
+    44 binariasTodas[g]
+                    *              1             0             1 
+    45 binariasTodas[h]
+                    *              0             0             1 
+    46 binariasTodas[i]
+                    *              0             0             1 
+    47 binariasTodas[j]
+                    *              0             0             1 
+    48 binariasTodas[notA]
+                    *              1             0             1 
+    49 binariasTodas[notB]
+                    *              1             0             1 
+    50 binariasTodas[notC]
+                    *              0             0             1 
+    51 binariasTodas[notD]
+                    *              1             0             1 
+    52 binariasTodas[notE]
+                    *              0             0             1 
+    53 binariasTodas[notF]
+                    *              0             0             1 
+    54 binariasTodas[notG]
+                    *              0             0             1 
+    55 binariasTodas[notH]
+                    *              1             0             1 
+    56 binariasTodas[notI]
+                    *              1             0             1 
+    57 binariasTodas[notJ]
+                    *              1             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
