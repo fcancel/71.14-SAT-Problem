b6bfe9f366d301bc18fcfca12c395eb613943da9
Fernando Cancellara <fdcancellara@gmail.com>
2016-10-12 17:35:10 -0300

    Subo output de tpAlt.mod

---
+++ tpAlt.sol
@@ -0,0 +1,307 @@
+Problem:    tpAlt
+Rows:       85
+Columns:    57 (57 integer, 57 binary)
+Non-zeros:  349
+Status:     INTEGER OPTIMAL
+Objective:  z = 37 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 z                          37                             
+     2 condicionesOrLadoIzquierdo[condicion1]
+                                   0                          -0 
+     3 condicionesOrLadoIzquierdo[condicion2]
+                                  -1                          -0 
+     4 condicionesOrLadoIzquierdo[condicion3]
+                                   0                          -0 
+     5 condicionesOrLadoIzquierdo[condicion4]
+                                   0                          -0 
+     6 condicionesOrLadoIzquierdo[condicion5]
+                                  -1                          -0 
+     7 condicionesOrLadoIzquierdo[condicion6]
+                                   0                          -0 
+     8 condicionesOrLadoIzquierdo[condicion7]
+                                   0                          -0 
+     9 condicionesOrLadoIzquierdo[condicion8]
+                                  -1                          -0 
+    10 condicionesOrLadoIzquierdo[condicion9]
+                                   0                          -0 
+    11 condicionesOrLadoIzquierdo[condicion10]
+                                   0                          -0 
+    12 condicionesOrLadoIzquierdo[condicion11]
+                                  -1                          -0 
+    13 condicionesOrLadoIzquierdo[condicion12]
+                                   0                          -0 
+    14 condicionesOrLadoIzquierdo[condicion13]
+                                  -1                          -0 
+    15 condicionesOrLadoIzquierdo[condicion14]
+                                  -1                          -0 
+    16 condicionesOrLadoIzquierdo[condicion15]
+                                  -1                          -0 
+    17 condicionesOrLadoIzquierdo[condicion16]
+                                  -2                          -0 
+    18 condicionesOrLadoIzquierdo[condicion17]
+                                  -1                          -0 
+    19 condicionesOrLadoIzquierdo[condicion18]
+                                   0                          -0 
+    20 condicionesOrLadoIzquierdo[condicion19]
+                                  -1                          -0 
+    21 condicionesOrLadoIzquierdo[condicion20]
+                                  -1                          -0 
+    22 condicionesOrLadoIzquierdo[condicion21]
+                                  -1                          -0 
+    23 condicionesOrLadoIzquierdo[condicion22]
+                                  -1                          -0 
+    24 condicionesOrLadoIzquierdo[condicion23]
+                                  -1                          -0 
+    25 condicionesOrLadoIzquierdo[condicion24]
+                                   0                          -0 
+    26 condicionesOrLadoIzquierdo[condicion25]
+                                  -1                          -0 
+    27 condicionesOrLadoIzquierdo[condicion26]
+                                  -1                          -0 
+    28 condicionesOrLadoIzquierdo[condicion27]
+                                  -2                          -0 
+    29 condicionesOrLadoIzquierdo[condicion28]
+                                  -1                          -0 
+    30 condicionesOrLadoIzquierdo[condicion29]
+                                  -2                          -0 
+    31 condicionesOrLadoIzquierdo[condicion30]
+                                  -1                          -0 
+    32 condicionesOrLadoIzquierdo[condicion31]
+                                  -1                          -0 
+    33 condicionesOrLadoIzquierdo[condicion32]
+                                  -1                          -0 
+    34 condicionesOrLadoIzquierdo[condicion33]
+                                  -1                          -0 
+    35 condicionesOrLadoIzquierdo[condicion34]
+                                  -1                          -0 
+    36 condicionesOrLadoIzquierdo[condicion35]
+                                  -1                          -0 
+    37 condicionesOrLadoIzquierdo[condicion36]
+                                  -1                          -0 
+    38 condicionesOrLadoIzquierdo[condicion37]
+                                  -1                          -0 
+    39 condicionesOrLadoDerecho[condicion1]
+                                  -2                          -0 
+    40 condicionesOrLadoDerecho[condicion2]
+                                  -1                          -0 
+    41 condicionesOrLadoDerecho[condicion3]
+                                  -2                          -0 
+    42 condicionesOrLadoDerecho[condicion4]
+                                  -2                          -0 
+    43 condicionesOrLadoDerecho[condicion5]
+                                  -1                          -0 
+    44 condicionesOrLadoDerecho[condicion6]
+                                  -2                          -0 
+    45 condicionesOrLadoDerecho[condicion7]
+                                  -2                          -0 
+    46 condicionesOrLadoDerecho[condicion8]
+                                  -1                          -0 
+    47 condicionesOrLadoDerecho[condicion9]
+                                  -2                          -0 
+    48 condicionesOrLadoDerecho[condicion10]
+                                  -2                          -0 
+    49 condicionesOrLadoDerecho[condicion11]
+                                  -1                          -0 
+    50 condicionesOrLadoDerecho[condicion12]
+                                  -2                          -0 
+    51 condicionesOrLadoDerecho[condicion13]
+                                  -1                          -0 
+    52 condicionesOrLadoDerecho[condicion14]
+                                  -1                          -0 
+    53 condicionesOrLadoDerecho[condicion15]
+                                  -1                          -0 
+    54 condicionesOrLadoDerecho[condicion16]
+                                   0                          -0 
+    55 condicionesOrLadoDerecho[condicion17]
+                                  -1                          -0 
+    56 condicionesOrLadoDerecho[condicion18]
+                                  -2                          -0 
+    57 condicionesOrLadoDerecho[condicion19]
+                                  -1                          -0 
+    58 condicionesOrLadoDerecho[condicion20]
+                                  -1                          -0 
+    59 condicionesOrLadoDerecho[condicion21]
+                                  -1                          -0 
+    60 condicionesOrLadoDerecho[condicion22]
+                                  -1                          -0 
+    61 condicionesOrLadoDerecho[condicion23]
+                                  -1                          -0 
+    62 condicionesOrLadoDerecho[condicion24]
+                                  -1                          -0 
+    63 condicionesOrLadoDerecho[condicion25]
+                                  -1                          -0 
+    64 condicionesOrLadoDerecho[condicion26]
+                                   0                          -0 
+    65 condicionesOrLadoDerecho[condicion27]
+                                   0                          -0 
+    66 condicionesOrLadoDerecho[condicion28]
+                                  -1                          -0 
+    67 condicionesOrLadoDerecho[condicion29]
+                                   0                          -0 
+    68 condicionesOrLadoDerecho[condicion30]
+                                  -1                          -0 
+    69 condicionesOrLadoDerecho[condicion31]
+                                  -1                          -0 
+    70 condicionesOrLadoDerecho[condicion32]
+                                  -1                          -0 
+    71 condicionesOrLadoDerecho[condicion33]
+                                  -1                          -0 
+    72 condicionesOrLadoDerecho[condicion34]
+                                  -1                          -0 
+    73 condicionesOrLadoDerecho[condicion35]
+                                  -1                          -0 
+    74 condicionesOrLadoDerecho[condicion36]
+                                  -1                          -0 
+    75 condicionesOrLadoDerecho[condicion37]
+                                  -1                          -0 
+    76 relacionarNormalesConSuNegada[a]
+                                   1             1             = 
+    77 relacionarNormalesConSuNegada[b]
+                                   1             1             = 
+    78 relacionarNormalesConSuNegada[c]
+                                   1             1             = 
+    79 relacionarNormalesConSuNegada[d]
+                                   1             1             = 
+    80 relacionarNormalesConSuNegada[e]
+                                   1             1             = 
+    81 relacionarNormalesConSuNegada[f]
+                                   1             1             = 
+    82 relacionarNormalesConSuNegada[g]
+                                   1             1             = 
+    83 relacionarNormalesConSuNegada[h]
+                                   1             1             = 
+    84 relacionarNormalesConSuNegada[i]
+                                   1             1             = 
+    85 relacionarNormalesConSuNegada[j]
+                                   1             1             = 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 condicionesACumplir[condicion1]
+                    *              1             0             1 
+     2 condicionesACumplir[condicion2]
+                    *              1             0             1 
+     3 condicionesACumplir[condicion3]
+                    *              1             0             1 
+     4 condicionesACumplir[condicion4]
+                    *              1             0             1 
+     5 condicionesACumplir[condicion5]
+                    *              1             0             1 
+     6 condicionesACumplir[condicion6]
+                    *              1             0             1 
+     7 condicionesACumplir[condicion7]
+                    *              1             0             1 
+     8 condicionesACumplir[condicion8]
+                    *              1             0             1 
+     9 condicionesACumplir[condicion9]
+                    *              1             0             1 
+    10 condicionesACumplir[condicion10]
+                    *              1             0             1 
+    11 condicionesACumplir[condicion11]
+                    *              1             0             1 
+    12 condicionesACumplir[condicion12]
+                    *              1             0             1 
+    13 condicionesACumplir[condicion13]
+                    *              1             0             1 
+    14 condicionesACumplir[condicion14]
+                    *              1             0             1 
+    15 condicionesACumplir[condicion15]
+                    *              1             0             1 
+    16 condicionesACumplir[condicion16]
+                    *              1             0             1 
+    17 condicionesACumplir[condicion17]
+                    *              1             0             1 
+    18 condicionesACumplir[condicion18]
+                    *              1             0             1 
+    19 condicionesACumplir[condicion19]
+                    *              1             0             1 
+    20 condicionesACumplir[condicion20]
+                    *              1             0             1 
+    21 condicionesACumplir[condicion21]
+                    *              1             0             1 
+    22 condicionesACumplir[condicion22]
+                    *              1             0             1 
+    23 condicionesACumplir[condicion23]
+                    *              1             0             1 
+    24 condicionesACumplir[condicion24]
+                    *              1             0             1 
+    25 condicionesACumplir[condicion25]
+                    *              1             0             1 
+    26 condicionesACumplir[condicion26]
+                    *              1             0             1 
+    27 condicionesACumplir[condicion27]
+                    *              1             0             1 
+    28 condicionesACumplir[condicion28]
+                    *              1             0             1 
+    29 condicionesACumplir[condicion29]
+                    *              1             0             1 
+    30 condicionesACumplir[condicion30]
+                    *              1             0             1 
+    31 condicionesACumplir[condicion31]
+                    *              1             0             1 
+    32 condicionesACumplir[condicion32]
+                    *              1             0             1 
+    33 condicionesACumplir[condicion33]
+                    *              1             0             1 
+    34 condicionesACumplir[condicion34]
+                    *              1             0             1 
+    35 condicionesACumplir[condicion35]
+                    *              1             0             1 
+    36 condicionesACumplir[condicion36]
+                    *              1             0             1 
+    37 condicionesACumplir[condicion37]
+                    *              1             0             1 
+    38 binariasTodas[normal,a]
+                    *              1             0             1 
+    39 binariasTodas[normal,b]
+                    *              0             0             1 
+    40 binariasTodas[normal,c]
+                    *              1             0             1 
+    41 binariasTodas[normal,d]
+                    *              0             0             1 
+    42 binariasTodas[normal,e]
+                    *              1             0             1 
+    43 binariasTodas[normal,f]
+                    *              1             0             1 
+    44 binariasTodas[normal,g]
+                    *              1             0             1 
+    45 binariasTodas[normal,h]
+                    *              0             0             1 
+    46 binariasTodas[normal,i]
+                    *              1             0             1 
+    47 binariasTodas[normal,j]
+                    *              0             0             1 
+    48 binariasTodas[negada,a]
+                    *              0             0             1 
+    49 binariasTodas[negada,b]
+                    *              1             0             1 
+    50 binariasTodas[negada,c]
+                    *              0             0             1 
+    51 binariasTodas[negada,d]
+                    *              1             0             1 
+    52 binariasTodas[negada,e]
+                    *              0             0             1 
+    53 binariasTodas[negada,f]
+                    *              0             0             1 
+    54 binariasTodas[negada,g]
+                    *              0             0             1 
+    55 binariasTodas[negada,h]
+                    *              1             0             1 
+    56 binariasTodas[negada,i]
+                    *              0             0             1 
+    57 binariasTodas[negada,j]
+                    *              1             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
